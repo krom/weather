8c069a94fa6fe4f2e80188f144539f6834c38bdc
Кудлай Роман <roman.kudlay@softline.ru>
2012-06-14 19:16:45 +0400

    Класс для работы с БД

---
+++ htdocs/lib/db.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: krom
+ * Date: 14.06.12
+ * Time: 19:10
+ * To change this template use File | Settings | File Templates.
+ */
+class db
+{
+ public static function getConnection() {
+
+ }
+}
