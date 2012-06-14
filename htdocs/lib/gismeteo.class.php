83c5fcd92f2e6cdaaa68085cabee040c4df5f9ac
Rustam Ibragimov <Rustam.Ibragimov@softline.ru>
2012-06-14 19:09:38 +0400

    методы созданы

--- htdocs/lib/gismeteo.class.php
+++ htdocs/lib/gismeteo.class.php
@@ -15,7 +15,10 @@ class gismeteo
      * @return int ИД города
      */
     function getCityIdByName($sName) {
-
+        $page = file_get_contents("http://www.gismeteo.ru/ajax/suggest/?callback=jQuery15104299549029674381_1339680183670&lang=ru&startsWith=$sName&sort=typ&_=1339681505875");
+        preg_match('/\(([^)]+)/i', $page, $m);
+        $info = json_decode($m[1], true);
+        return $info ['items'][0]['id'];
     }
 
     /**
@@ -26,6 +29,16 @@ class gismeteo
      * [pressure] - давление
      */
     function getWeatherByCityId($iCityId) {
-
+        $page = file_get_contents("http://www.gismeteo.ru/city/daily/$iCityId/");
+        if (preg_match('/Погода за окном.*?<dd class=\'value m_temp c\'>([+0-9]+).*?<dd class=\'value m_press torr\'>(\d+)/isu', $page, $m)) {
+            $weather_array [temperature] = $m[1];
+            $weather_array [pressure] = $m[2];
+        }
     }
 }
+
+
+
+
+
+
