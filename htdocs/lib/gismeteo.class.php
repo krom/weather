988d96f9df8e8ec16eb6a62c287d2945f4dbea46
Кудлай Роман <roman.kudlay@softline.ru>
2012-06-14 18:53:32 +0400

    new version

---
+++ htdocs/lib/gismeteo.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: krom
+ * Date: 14.06.12
+ * Time: 18:50
+ * To change this template use File | Settings | File Templates.
+ */
+class gismeteo
+{
+
+    function getCityIdByName($sName) {
+
+    }
+
+    /**
+     * @param $iCityId
+     * @return array
+     * [temperature]
+     * [pressure]
+     */
+    function getWeatherByCityId($iCityId) {
+
+    }
+}
